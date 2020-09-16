6c019d6066ca03406215644f2960da0e6165d962
AliSalem01 <71129604+AliSalem01@users.noreply.github.com>
2020-09-16 12:41:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 int main()
+  #include <iostream>
+  #include "function.h"
+  using namesapce std;
 {
 int val[] = { 60, 100, 120 };
 int wt[] = { 10, 20, 30 };
@@ -6,3 +9,4 @@ int W = 50;
 int n = sizeof(val) / sizeof(val[0]);
 cout << knapSack(W, wt, val, n);
 return 0;
+}
